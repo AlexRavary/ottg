1cc5f61a62cdc52c9c9b38dc2b5aa4ff8737cc4f
Alex Ravary <alex.ravary@gmail.com>
2020-12-07 23:08:05 -0500

    Shifted item creation to list.html Added ToDo items Moved tests around to match new structure. Cleaned up unnecessary tests. Simplified home page view Added new_list view for new items.

--- lists/models.py
+++ lists/models.py
@@ -4,3 +4,5 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     text = models.TextField(default='')
+
+# ToDo: Adjust model so that items are associated with different lists
